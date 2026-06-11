afc1442fa09d3d13074a42d05c6280ef9213b823
AJ <alfcabrales@gmail.com>
2026-06-10 23:27:18 -0600

    Modules lab completed

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+require('datejs');
 
+/**
+ * Combinearrays of usernames 
+ * return an object with the merged users + merge date.
+ * 
+ * @param {...Array<string>} args 
+ * @returns {Object} { users: string[], merge_date: string }
+ */
+function combineUsers(...args) {
+    // Initialize the return object
+    const combinedObject = {
+        users: []
+    };
 
+    // Loop through each array and merge 
+    for (const userArray of args) {
+        if (Array.isArray(userArray)) {
+            combinedObject.users.push(...userArray);
+        }
+    }
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+    // Add today's date in M/d/yyyy format
+    combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+    return combinedObject;
+}
+
+module.exports = { combineUsers };
